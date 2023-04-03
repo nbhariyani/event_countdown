3a3d5ff40c7c9bbb4a2f03ef12017032e775b5ed
naresh.hariyani <naresh.hariyani@laminaar.in>
2023-04-03 20:22:47 +0530

    diff condition added.

--- script.js
+++ script.js
@@ -17,19 +17,19 @@ const newYearTime = new Date(`02-APR-2023 06:45`);
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
+  if (diff > 0) {
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
 
-  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-  const m = Math.floor(diff / 1000 / 60) % 60;
-  const s = Math.floor(diff / 1000) % 60;
-
-  // Add values to DOM
-  days.innerHTML = d;
-  hours.innerHTML = h < 10 ? "0" + h : h;
-  minutes.innerHTML = m < 10 ? "0" + m : m;
-  seconds.innerHTML = s < 10 ? "0" + s : s;
+    // Add values to DOM
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? "0" + h : h;
+    minutes.innerHTML = m < 10 ? "0" + m : m;
+    seconds.innerHTML = s < 10 ? "0" + s : s;
+  }
 }
-
 // Show spinner before countdown
 setTimeout(() => {
   loading.remove();
